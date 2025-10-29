65b27df9879dda1af76c6dcb6a57448d73250954
Jai Satya Abhiram Mallidi <163704204+jaisatyaabhiram@users.noreply.github.com>
2025-10-29 20:04:48 +0530

    Refactor character stuffing logic in pro2a.c
    Fixed character stuffing implementation and syntax errors.

--- pro2a.c
+++ pro2a.c
@@ -1,37 +1,32 @@
+//Character stuffing 
+
 #include<stdio.h>
- #include<string.h>
+#include<string.h>
 void main()
 {
-int a[20],b[30],i,j,k,count,n; 
-printf("Enter frame length:");
- scanf("%d",&n);
-printf("Enter input frame (0's & 1's only):");
-for(i=0;i<n;i++) 
-scanf("%d",&a[i]);
- i=0; count=1; j=0;
- while(i<n)
-{
-if(a[i]==1) 
-b[j]=a[i]; 
-for(k=i+1;a[k]==1&& k<n && count<5;k++)
-{
-j++; 
-count++; 
-if(count==5)
-{
-j++; 
-b[j]=0; 
+char a[30], fs[50]="", t[3], sd, ed, x[3], s[3], d[3], [3];
+int i, j, p = 0 q = 0
+printf("Enter characters to be stuffed:");
+scanf("%s", a);
+printf("\nEnter a character that represents starting delimiter:");
+scanf("%c", &sd);
+printf("\nEnter a character that represents ending delirniter:");
+scanf("%c", &ed);
+x[O] = s[O] = s[1] = sd
+x[1] = s[2] = 10
+y[O] = d[O] = d[1] = ed
+d[2] = y[1] ='/0' ;
+strcat(fs, x);
+for(i=0; i < strlen(a); i++) {
+t[O] = a[i];
+t[1] = '/0';
+if( t[0] ==sd)
+strcat(fs, s);
+else if(t[0]==e d)
+strcat(fs, d);
+else
+strcat(fs, t);
 }
-i-k;
-}}
- else
-{
-b[j]=a[i];
-}
-I++;
-j++;
-}
-printf("After stuffing the frame is:");
-for(i=0;i<j;i++)
-printf("%d",b[i]);
+strcat(fs, y);
+printf("\n After stuffing:%s", fs);
 }
